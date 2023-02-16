f8011f606b9732cac005a5ff3795a2c05833a5bd
Galdino13 <59259616+Galdino13@users.noreply.github.com>
2023-02-16 16:31:01 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <div className="overlay"></div>
+      <div className="overlay2">
+        <h1>BEM VINDO AO INTERPRETA!</h1>
+        <h2>O melhor app para aprender interpretação de texto!</h2>
+        <div className="cadastro">Cadastro</div>
+        <div className="login">Login</div>
+        <button className="cada2"></button>
+        <button className="logi2"></button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
